1aa5b4cf5b16e8fefc743ac39eb627705b127c7c
apJammanbo <apjammanbo.gmail.com>
2019-09-23 06:02:03 +0900

    필요없는파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>반갑습니다. 강원대학교 학생여러분!</p>
       </header>
     </div>
