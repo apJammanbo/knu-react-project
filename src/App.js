97baabfa8eaa397eba43df2bae0dc58664e9908e
apJammanbo <apjammanbo.gmail.com>
2019-09-23 05:53:47 +0900

    인사말 추가

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>반갑습니다. 강원대학교 학생여러분!</p>
       </header>
     </div>
   );
